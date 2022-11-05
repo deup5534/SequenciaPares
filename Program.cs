eecd0ffc578f626d6a8ae2081a1a6c55dc873042
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-04 21:47:57 -0300

    foi legal fazer

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("\nexibir números pares de 0 até...");
+int numeroInicial = Convert.ToInt32(Console.ReadLine());
+
+if (numeroInicial >=1)
+{
+    int numeroPar = 0;
+
+    while (numeroPar <= numeroInicial)
+    {
+        Console.Write($"{numeroPar} ");
+        numeroPar += 2;
+    }
+}
+Console.WriteLine();
